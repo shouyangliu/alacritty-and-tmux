f6106cb2900fa773162ebc435956413cb5b3a7d4
yang-ubuntu <tylerliuyang@outlook.com>
2023-11-28 00:23:52 +0800

    first commit

---
+++ install.sh
@@ -0,0 +1,3 @@
+# We use Alacritty's default Linux config directory as our storage location here.
+mkdir -p ~/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
